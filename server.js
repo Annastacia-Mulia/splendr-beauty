98fe2446242d459188ca14127f272986c67e71b0
annaaaa <annastacia.nzoka@strathmore.edu>
2024-06-20 15:58:08 +0300

    database

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const crypto = require("crypto");
 const path = require("path");
 //const multer = require("multer");
 
-
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
