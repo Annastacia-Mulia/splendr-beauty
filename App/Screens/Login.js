01e233c9845ce49face3832dd4cb5133185f9800
Shikulisa <lisa.wanjiku@strathmoree.com>
2024-07-12 10:46:24 +0300

    Login

---
+++ App/Screens/Login.js
@@ -0,0 +1,243 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import axios from "axios";
+import Config from "../Server/config";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
+
+export default function Login({ navigation }) {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`${Config.API_URL}/api/login`, {
+        email: form.email,
+        password: form.password,
+      });
+
+      if (response.status === 200) {
+        // Store user ID and userType in AsyncStorage
+        await AsyncStorage.setItem("userID", response.data.userID.toString());
+        await AsyncStorage.setItem("userType", response.data.userType);
+
+        Alert.alert("Success", "Login successful");
+
+        // Redirect to the respective home screen based on userType
+        if (response.data.userType === "Client") {
+          navigation.navigate("ClientHomeScreen");
+        } else if (response.data.userType === "Beautician") {
+          navigation.navigate("BeauticianHomeScreen");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.status === 401) {
+        Alert.alert("Error", "Invalid credentials");
+      } else {
+        Alert.alert("Error", "An error occurred. Please try again.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#e8ecf4" }}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              Login to <Text style={{ color: "#075eec" }}>Splendr</Text>
+            </Text>
+            <Text style={styles.subtitle}>
+              The Ultimate Beauty booking application
+            </Text>
+          </View>
+
+          <View style={styles.form}>
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Email address</Text>
+              <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                clearButtonMode="while-editing"
+                keyboardType="email-address"
+                onChangeText={(email) => setForm({ ...form, email })}
+                placeholder="name@gmail.com"
+                placeholderTextColor="#6b7280"
+                style={styles.inputControl}
+                value={form.email}
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Password</Text>
+              <TextInput
+                autoCorrect={false}
+                clearButtonMode="while-editing"
+                onChangeText={(password) => setForm({ ...form, password })}
+                placeholder="********"
+                placeholderTextColor="#6b7280"
+                style={styles.inputControl}
+                secureTextEntry={!showPassword}
+                value={form.password}
+              />
+              <TouchableOpacity
+                style={styles.passwordVisibilityIcon}
+                onPress={togglePasswordVisibility}
+              >
+                <Text>{showPassword ? "Hide" : "Show"}</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.formAction}>
+              <TouchableOpacity onPress={handleLogin}>
+                <View style={styles.btn}>
+                  {isLoading ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : (
+                    <Text style={styles.btnText}>Login</Text>
+                  )}
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ForgotPassword")}
+            >
+              <Text style={styles.formLink}>Forgot password?</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Signup")}
+            style={{ marginTop: "auto" }}
+          >
+            <Text style={styles.formFooter}>
+              Don't have an account?{" "}
+              <Text style={{ textDecorationLine: "underline" }}>Sign up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  container: {
+    paddingVertical: 24,
+    paddingHorizontal: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  title: {
+    fontSize: 31,
+    fontWeight: "700",
+    color: "#1D2A32",
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#929292",
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 36,
+  },
+  form: {
+    marginBottom: 24,
+    paddingHorizontal: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  formAction: {
+    marginTop: 4,
+    marginBottom: 16,
+  },
+  formLink: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#075eec",
+    textAlign: "center",
+  },
+  formFooter: {
+    fontSize: 15,
+    fontWeight: "600",
+    color: "#222",
+    textAlign: "center",
+    letterSpacing: 0.15,
+  },
+  input: {
+    marginBottom: 16,
+    position: "relative",
+  },
+  inputLabel: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "#222",
+    marginBottom: 8,
+  },
+  inputControl: {
+    height: 50,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#222",
+    borderWidth: 1,
+    borderColor: "#C9D3DB",
+    borderStyle: "solid",
+  },
+  btn: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: "#075eec",
+    borderColor: "#075eec",
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: "600",
+    color: "#fff",
+  },
+  passwordVisibilityIcon: {
+    position: "absolute",
+    right: 16,
+    top: "50%",
+    transform: [{ translateY: -8 }],
+  },
+});
